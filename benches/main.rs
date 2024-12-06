0ef3918848f32b4974a5c146fd4bf724bb81232e
mohanson <mohanson@outlook.com>
2024-12-06 09:54:58 +0800

    Use random data for benching

--- benches/main.rs
+++ benches/main.rs
@@ -1,25 +1,27 @@
-fn bench_blake2b_rs(c: &mut criterion::Criterion) {
-    let data = [0; 4096];
+use rand::Rng;
+
+fn bench_main(c: &mut criterion::Criterion) {
+    let mut rng = rand::thread_rng();
+    let mut buf = [0; 4096];
+    rng.fill(&mut buf);
+
     c.bench_function("bench_blake2b_rs", |b| {
         b.iter(|| {
             let mut h = blake2b_rs::Blake2bBuilder::new(32).personal(b"ckb-default-hash").build();
-            h.update(&data);
+            h.update(&buf);
             let mut r = [0; 32];
             h.finalize(&mut r);
             criterion::black_box(r);
         })
     });
-}
 
-fn bench_blake2ya(c: &mut criterion::Criterion) {
-    let data = [0; 4096];
     c.bench_function("bench_blake2ya", |b| {
         b.iter(|| {
             let mut p = blake2ya::blake2b_params();
             p.digest(32);
             p.person(b"ckb-default-hash");
             let mut h = blake2ya::blake2b(p);
-            h.update(&data);
+            h.update(&buf);
             let mut r = [0; 32];
             h.digest(&mut r);
             criterion::black_box(r);
@@ -27,5 +29,5 @@ fn bench_blake2ya(c: &mut criterion::Criterion) {
     });
 }
 
-criterion::criterion_group!(benches, bench_blake2b_rs, bench_blake2ya,);
+criterion::criterion_group!(benches, bench_main);
 criterion::criterion_main!(benches);
